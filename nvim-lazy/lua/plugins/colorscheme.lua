8f689aa9dade3cf5a83712df83e93c4faed46c91
Romain Foyard <rfoyard@gmail.com>
2025-01-21 15:06:12 +0100

    updated neovim theme

--- nvim-lazy/lua/plugins/colorscheme.lua
+++ nvim-lazy/lua/plugins/colorscheme.lua
@@ -3,7 +3,22 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "savq/melange-nvim" },
   { "rebelot/kanagawa.nvim" },
-  { "AlexvZyl/nordic.nvim" },
+
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").load({
+        cursorline = {
+          blend = 0.6,
+        },
+        telescope = {
+          style = "classic",
+        },
+      })
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
