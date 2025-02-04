40ea98078fa9f5f5e87a87da1c55b89f5884c6e0
Romain Foyard <rfoyard@gmail.com>
2025-02-04 16:09:00 +0100

    added kubectl to fish tide prompt

--- fish/functions/_tide_item_kubectl.fish
+++ fish/functions/_tide_item_kubectl.fish
@@ -1,4 +1,14 @@
 function _tide_item_kubectl
-    kubectl config view --minify --output 'jsonpath={.current-context}/{..namespace}' 2>/dev/null | read -l context &&
+    kubectl config view --minify --output 'jsonpath={.current-context}' 2>/dev/null | read -l context
+    if string match -rq '^gke_.*_(?P<cluster>[\\w-]+)' $context
+        set color $tide_kubectl_color
+        if test $cluster = evy-sandbox
+            set color yellow
+        else if test $cluster = evy-production
+            set color red
+        end
+        tide_kubectl_color=$color _tide_print_item kubectl $tide_kubectl_icon' ' (string replace -r '/(|default)$' '' $cluster)
+    else
         _tide_print_item kubectl $tide_kubectl_icon' ' (string replace -r '/(|default)$' '' $context)
+    end
 end
